a082830cb5b85569bd759f9f2b097f27b458e731
pobortcevv <pobortcevv@gmail.com>
2020-11-09 23:20:29 +0300

    gg

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: sabra <sabra@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 17:25:08 by sabra             #+#    #+#             */
-/*   Updated: 2020/11/09 20:40:57 by sabra            ###   ########.fr       */
+/*   Updated: 2020/11/09 23:20:01 by sabra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	i = 0;
 	if (little[i] == '\0')
 		return ((char*)big);
-	while (len-- && *(big++) != '\0')
+	while (len && *big != '\0')
 	{
 		if (*big == *little)
 		{
-			while (big[i] == little[i] && big[i] != '\0')
+			while (big[i] == little[i] && big[i] != '\0' && len--)
 				i++;
 			if (little[i] == '\0')
 				return ((char*)big);
 			i = 0;
 		}
+		big++;
+		len--;
 	}
 	return (NULL);
 }
